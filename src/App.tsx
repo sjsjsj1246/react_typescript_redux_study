4f76d63b0b3e9e7f329a48742b53a40b4501bb01
Hwang In Seo <sjsjsj1246@gmail.com>
2021-03-30 20:55:57 +0900

    Feature: ContextAPI with ts

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,14 @@ import Counter from "./Counter";
 import Greetings from "./Greetings";
 import MyForm from "./MyForm";
 import ReducerSample from "./ReducerSample";
+import { SampleProvider } from "./SampleContext";
 
 const App: React.FC = () => {
-  return <ReducerSample />;
+  return (
+    <SampleProvider>
+      <ReducerSample />
+    </SampleProvider>
+  );
 };
 
 export default App;
